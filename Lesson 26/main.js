d043fba1d648959031a47eedc2693a6c0d3216ca
eneshaliti16 <halitienes95@gmail.com>
2026-03-24 19:19:04 +0100

    javascript start

---
+++ Lesson 26/main.js
@@ -0,0 +1,62 @@
+console.log("HEllo");
+
+var x = 5 * 2352345;
+
+console.log(x);
+
+var a=13123;
+var b= 123124;
+var c= a-b;
+console.log(c);
+
+var h=13123;
+var i= 123124;
+var j= h/i;
+console.log(j);
+
+var k=13123;
+var l= 123124;
+var m= a*b;
+console.log(m);
+
+var n=13123;
+var o= 123124;
+var p= a%b;
+console.log(p);
+
+var q= 90;
+q++;
+console.log(q)
+
+var g = 90;
+g--;
+console.log(g)
+
+var f = 90;
+var x = f** 2;
+console.log(x)
+
+
+var l = 15;
+l +=5;
+console.log(l)
+
+var la = 15;
+la -=5;
+console.log(la)
+
+var ld = 15;
+ld /=5;
+console.log(ld)
+
+var ab = 5;
+var ac = "1234";
+
+console.log(ab > ac);
+console.log(ab < ac);
+console.log(ab == ac);
+
+var name = "filan";
+var surname = "fisteku";
+
+console.log(name+" "+surname)
