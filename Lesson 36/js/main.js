e9cd1267ad6f2e0159fbedda938b07fc2269be97
eneshaliti16 <halitienes95@gmail.com>
2026-05-12 19:18:03 +0200

    Lesson 36

---
+++ Lesson 36/js/main.js
@@ -0,0 +1,19 @@
+// $(window).ready(function(){
+//     alert('It is working');
+// })
+
+$('li').click(function(){
+    alert('Clicked');
+});
+
+$('#btn').click(function(){
+    console.log($('#h1').text());
+    $('#h1').text('Testing test');
+    $('#h1').append('Extra test');
+});
+
+$('#myDiv').click(function(){
+    $(this).addClass('clicked');
+})
+.find('span')
+.attr('title','Hover over me');
