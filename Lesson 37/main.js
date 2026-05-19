ff75d3e7ed6b23bef86fba8a3fedc4a003ba2055
eneshaliti16 <halitienes95@gmail.com>
2026-05-19 19:12:13 +0200

    Lesson 37

---
+++ Lesson 37/main.js
@@ -0,0 +1,24 @@
+// $(function(){
+//     alert("hello");
+// });
+
+$('#sort').sortable();
+
+
+$(function(){
+    $('#accordion').accordion();
+});
+
+$(function(){
+    $('#accordion').accordion({
+        collapsible: true
+    });
+});
+
+$('#tabs').tabs();
+
+$(function(){
+    $('#tabs').tabs({
+        collapsible: true
+    });
+});
